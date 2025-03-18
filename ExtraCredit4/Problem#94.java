28ea6dc9ad4d4944174f8554e48a3983ac7f5394
Arnav Khetan <83293565+arnavakhetan@users.noreply.github.com>
2025-03-17 22:00:22 -0700

    Create Problem#94.java

---
+++ ExtraCredit4/Problem#94.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) { 
+                stack.push(current);  
+                current = current.left;
+            }
+            current = stack.pop(); 
+            result.add(current.val);
+            current = current.right; 
+        }
+        return result;
+    }
+}
